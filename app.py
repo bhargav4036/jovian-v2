1b2c77100d2f0b2956cbf34cb441eb24844c02f7
bhargav4036 <107092872+bhargav4036@users.noreply.github.com>
2023-06-08 09:10:57 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
-app=Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
 @app.route('/')
 def hello_world():
-  return "hello"
-app.run(host='0.0.0.0',debug=True)
+  return render_template('home.html')
+
+
+app.run(host='0.0.0.0', debug=True)
