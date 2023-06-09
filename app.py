5fdaf9de9463943e8219cff1aa3da7228b8afdbb
bhargav4036 <107092872+bhargav4036@users.noreply.github.com>
2023-06-09 06:07:58 +0000

    added css

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+JOBS = [{
+  'id': 1,
+  'title': 'Data engineer',
+  'location': 'delhi',
+  'salary': 'rs.150000'
+}, {
+  'id': 2,
+  'title': 'Data analyst',
+  'location': 'india',
+  'salary': 'rs.15000'
+}, {
+  'id': 3,
+  'title': 'Data manager',
+  'location': 'delhiii',
+  'salary': 'rs.15000'
+}]
 
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company='jovian')
+
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
 
 
 app.run(host='0.0.0.0', debug=True)
